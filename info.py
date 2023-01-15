fbb40a7f47a60f6f698acc30a95ec352a3df1637
Faznj <108731444+fazni7@users.noreply.github.com>
2023-01-15 15:43:21 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "DATABASE_URI', "mongodb+srv://fznkd:fznkd@cluster0.wifumu3.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://fznkd:fznkd@cluster0.wifumu3.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "TamilRockersMovies")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Ajax_Files')
 
